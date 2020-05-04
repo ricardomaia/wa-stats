a585b56b3350463013093ce7b342af5e3babf4be
rsmaia@gmail.com <nQeVcJcwuMc53nt>
2020-05-04 10:16:09 -0300

    word cloud setings.

--- commom_words.php
+++ commom_words.php
@@ -4,21 +4,21 @@ $sql = "
 SELECT lower(word) AS word, count(lower(word)) AS Total FROM stats 
 WHERE word NOT IN ('media')
 GROUP BY word ORDER BY Total DESC
-LIMIT 200
+LIMIT 300
 ";
 $results = $db->query($sql);
 $dataset = "";
 
 while ($row = $results->fetchArray()) {
 
-    $dataset .= "{\"tag\": \"{$row['word']}\", \"count\": \"{$row['Total']}\" },";
+    $dataset .= "{\"word\": \"{$row['word']}\", \"count\": \"{$row['Total']}\" },";
 }
 ?>
 <!-- Styles -->
 <style>
     #chartdiv {
         width: 100%;
-        height: 50vh;
+        height: 80vh;
     }
 </style>
 
@@ -39,29 +39,36 @@ while ($row = $results->fetchArray()) {
 
 
         var chart = am4core.create("chartdiv", am4plugins_wordCloud.WordCloud);
-        chart.fontFamily = "Courier New";
+        chart.fontFamily = "Corbel, Arial, Sans-Serif";
         var series = chart.series.push(new am4plugins_wordCloud.WordCloudSeries());
-        series.randomness = 0.1;
+
+        /*
+                series.events.on("arrangestarted", function(ev) {
+                    ev.target.baseSprite.preloader.show(0);
+                });
+
+                series.events.on("arrangeprogress", function(ev) {
+                    ev.target.baseSprite.preloader.progress = ev.progress;
+                });
+        */
+
+        series.randomness = 0.2;
         series.rotationThreshold = 0.5;
 
         series.data = [<?= $dataset ?>];
 
-
-        series.dataFields.word = "tag";
+        series.dataFields.word = "word";
         series.dataFields.value = "count";
+        series.colors = new am4core.ColorSet();
+        series.colors.passOptions = {};
 
-        series.heatRules.push({
-            "target": series.labels.template,
-            "property": "fill",
-            "min": am4core.color("#0000CC"),
-            "max": am4core.color("#CC00CC"),
-            "dataField": "value"
-        });
+        series.labels.template.tooltipText = "{word}:\n[bold]{count}[/]";
+
 
         var hoverState = series.labels.template.states.create("hover");
         hoverState.properties.fill = am4core.color("#FF0000");
         var title = chart.titles.create();
-        title.text = "TOP #100 common words";
+        title.text = "TOP common words";
         title.fontSize = 28;
         title.fontWeight = "800";
 
---
+++ word_cloud.php
@@ -0,0 +1,74 @@
+<?php
+
+$sql = "
+SELECT word FROM stats
+WHERE word IN (
+    SELECT word FROM (
+        SELECT word, count(word) as Total FROM stats
+        GROUP BY word
+        ORDER BY Total DESC
+        LIMIT 50
+    )   
+)
+";
+$results = $db->query($sql);
+$dataset = "";
+
+while ($row = $results->fetchArray()) {
+
+    //$dataset .= "{\"tag\": \"{$row['word']}\", \"count\": \"{$row['Total']}\" },";
+    $dataset .= "{$row['word']},";
+}
+?>
+<!-- Styles -->
+<style>
+    #chartdiv {
+        width: 100%;
+        height: 60vh;
+    }
+</style>
+
+<!-- Resources -->
+<script src="https://www.amcharts.com/lib/4/core.js"></script>
+<script src="https://www.amcharts.com/lib/4/charts.js"></script>
+<script src="https://www.amcharts.com/lib/4/plugins/wordCloud.js"></script>
+<script src="https://www.amcharts.com/lib/4/themes/animated.js"></script>
+
+<!-- Chart code -->
+<script>
+    am4core.ready(function() {
+
+        // Themes begin
+        am4core.useTheme(am4themes_animated);
+        // Themes end
+
+
+        var chart = am4core.create("chartdiv", am4plugins_wordCloud.WordCloud);
+        var series = chart.series.push(new am4plugins_wordCloud.WordCloudSeries());
+
+        series.accuracy = 4;
+        series.step = 15;
+        series.rotationThreshold = 0.7;
+        series.maxCount = 200;
+        series.minWordLength = 2;
+        series.labels.template.margin(4, 4, 4, 4);
+        series.maxFontSize = am4core.percent(30);
+
+        series.text = "<?= $dataset ?>";
+
+        series.colors = new am4core.ColorSet();
+        series.colors.passOptions = {}; // makes it loop
+
+        //series.labelsContainer.rotation = 45;
+        series.angles = [0, -90];
+        series.fontWeight = "700"
+
+        setInterval(function() {
+            series.dataItems.getIndex(Math.round(Math.random() * (series.dataItems.length - 1))).setValue("value", Math.round(Math.random() * 10));
+        }, 10000)
+
+    }); // end am4core.ready()
+</script>
+
+<!-- HTML -->
+<div id="chartdiv"></div>
